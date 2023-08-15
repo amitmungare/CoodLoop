75eb696d5d1bb502d9abb90a3e1158fc6eed6452
amitmungare <amungare27@gmail.com>
2023-08-15 20:25:34 +0530

    java variables and methods

---
+++ src/Code.java
@@ -0,0 +1,25 @@
+// class
+public class Code {
+
+    // method
+    public static void main(String[] args) {
+
+        // this is my print lines
+//        System.out.println("hello");
+//        System.out.println(23);
+//        System.out.println("ok");
+
+//        Variables
+        int d;
+        int a =1;
+        String s= "Hello";
+        char c = 'a';
+        boolean b = false;
+        System.out.println(s+s+s+"my");
+        System.out.println(a+a+a+15);
+
+
+    }
+
+
+}
