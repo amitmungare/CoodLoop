eba10940bb58c602fb05657bb7becaa5ed481265
amitmungare <amungare27@gmail.com>
2023-08-17 12:18:06 +0530

    Java Operators

--- src/Code.java
+++ src/Code.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // class
 public class Code {
 
@@ -10,13 +14,149 @@ public class Code {
 //        System.out.println("ok");
 
 //        Variables
-        int d;
-        int a =1;
-        String s= "Hello";
-        char c = 'a';
-        boolean b = false;
-        System.out.println(s+s+s+"my");
-        System.out.println(a+a+a+15);
+//        int d;
+//        int a =1;
+//        String s= "Hello";
+//        char c = 'a';
+//        boolean b = false;
+//        System.out.println(s+s+s+"my");
+//        System.out.println(a+a+a+15);
+
+//        Data Type
+//        int aa=7+7;
+//        String ss = "amit";
+//        System.out.println( ss.length() );
+
+//        Java Type Casting
+//        long x = 11111111;
+//        System.out.println(x);
+
+
+//        int myInt = 9;
+//        double myDouble = myInt;
+
+//        double dd = 9.0;
+//        int myint = (int) dd;
+//
+//        int xx = (int) 9.0;
+//
+//        System.out.println(xx);
+//
+
+//        Java Operators
+
+//        int a=10;
+//        int b=20;
+
+//        int x= a+b;
+//        int x= a-b;
+//        int x= a*b;
+//        int x= a/b;  //10/20 = 0.2 =0
+//        int x= b%a;
+//        a++;  //a+1
+//        b--; //b-1
+
+//        // a=10;
+//        int x = ++a; //a=11
+//        System.out.println("x1 = "+ x); //x=11
+//        x=a++; // x=11 , a=12
+//        System.out.println("x2 = "+ x);// x==11
+//        System.out.println("a = "+ a);// a=12
+
+//        // a=10;
+//        int x = a++; //x=10 , a=11
+//        System.out.println("x1 = "+ x); //x=10
+//        x= ++a; // x=12 , a=12
+//        System.out.println("x2 = "+ x);// x=12
+//        System.out.println("a = "+ a);// a=12
+
+//        // a=10;
+//        int x = a--; //x=10 , a=9
+//        System.out.println("x1 = "+ x++); //x=11
+//        System.out.println(x);
+//        x= --a; // x=8 , a=8
+//        System.out.println("x2 = "+ x);// x=8
+//        System.out.println("a = "+ a);// a=8
+
+
+//        Assignment Operators
+//        int x=15;
+
+//        x=5;//5
+//        x+=5; // x=x+5;
+//        x-=10; // x=x-10 => x=15-10 =>5
+//        x*=10;
+//        x/=10;
+//        x%=10;
+//        x&=10;
+//        x|=10;
+//        x^=10; //x=x^10 => 15^10
+//        x>>=10;
+//        <<=
+
+
+//        Comparison Operators
+
+//        int a = 10;
+//        int b=10;
+//
+//        System.out.println(a==b);
+//        System.out.println(a!=b);
+//        System.out.println(a<b); //5<10
+//        System.out.println(a>b);//5>10
+//        System.out.println(a<=b);
+//        System.out.println(a>=b);
+
+
+//        Logical Operators
+
+//        int a=10;
+//        int b=20;
+
+//        a==b || a==10
+//        false || true
+//
+//        a==10 || a==b
+//        true
+
+//        if(a==10 || a==b){
+//            System.out.println("yes.....");
+//        }else {
+//            System.out.println("noooo....");
+//        }
+
+//        a=10
+//        b=20
+//        if(a==10 && a==b){
+//            System.out.println("yes.....");
+//        }else {
+//            System.out.println("noooo....");
+//        }
+
+        //a=10
+//        if(!(a==11)){
+//            System.out.println("yes.....");
+//        }else {
+//            System.out.println("noooo....");
+//        }
+
+
+        // Initial values
+//        int a = 5;
+//        int b = 7;
+//        System.out.println(a|b);
+
+        // bitwise and
+        // 0101 & 0111=0101 = 5
+//        System.out.println("a&b = " + (a & b));
+//
+//        // bitwise or System.out.println("a|b = " + (a | b));
+//        // bitwise xor System.out.println("a^b = " + (a ^ b));
+//        // bitwise not  System.out.println("~a = " + ~a);
+//        // assignment operator to // a=a&b
+//        a &= b;
+//        System.out.println("a= " + a);
+
 
 
     }
